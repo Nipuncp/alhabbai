666b8ee73b226e522af8969d12f51fdbce643034
Nipuncp <nipuncp123@gmail.com>
2025-03-05 02:29:17 +0000

    Initial setup with job regitration doctype

---
+++ alhabbai/alhabbai/doctype/job_registration/job_registration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Nipun and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Job Registration", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ alhabbai/alhabbai/doctype/job_registration/test_job_registration.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Nipun and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestJobRegistration(FrappeTestCase):
+	pass
